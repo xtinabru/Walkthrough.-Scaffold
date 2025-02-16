0752e0b472785092bca21405e3a9dbc3dcf0244b
Cristofor4ik <xtinabruscaginxxx@gmail.com>
2025-02-17 00:25:17 +0200

    Add dropdown options

--- app/src/main/java/com/example/walkthroughscaffold/MainActivity.kt
+++ app/src/main/java/com/example/walkthroughscaffold/MainActivity.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -17,10 +19,15 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.walkthroughscaffold.ui.theme.WalkthroughScaffoldTheme
+import kotlin.math.exp
 
 class MainActivity : ComponentActivity() {
     @OptIn (ExperimentalMaterial3Api::class)
@@ -31,6 +38,7 @@ class MainActivity : ComponentActivity() {
             WalkthroughScaffoldTheme {
                 Scaffold(
                     topBar = {
+                        var expanded by remember { mutableStateOf(false)}
                         TopAppBar(
                             colors = TopAppBarDefaults.topAppBarColors(
                                 containerColor = MaterialTheme.colorScheme.primaryContainer,
@@ -48,10 +56,23 @@ class MainActivity : ComponentActivity() {
                                 }
                             },
                             actions = {
-                                IconButton(onClick = { /* logic*/}) {
+                                IconButton(onClick = { expanded =!expanded}) {
                                     Icon(
                                         imageVector = Icons.Filled.MoreVert,
                                         contentDescription = "Localized description"
+                                    )
+                                }
+                                DropdownMenu(
+                                    expanded = expanded,
+                                    onDismissRequest = { expanded = false}
+                                ) {
+                                    DropdownMenuItem(
+                                        text = {Text("Info")},
+                                        onClick = { /* logic*/}
+                                    )
+                                    DropdownMenuItem(
+                                        text = {Text("Settings")},
+                                        onClick = { /* logic*/}
                                     )
                                 }
                             }
