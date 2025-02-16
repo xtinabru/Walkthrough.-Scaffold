6039b4caedf3efa9ba58bdc7df8938c33dc00a77
Cristofor4ik <xtinabruscaginxxx@gmail.com>
2025-02-17 00:13:38 +0200

    Add icons into TopAppBar

--- app/src/main/java/com/example/walkthroughscaffold/MainActivity.kt
+++ app/src/main/java/com/example/walkthroughscaffold/MainActivity.kt
@@ -5,7 +5,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -33,6 +38,22 @@ class MainActivity : ComponentActivity() {
                             ),
                             title = {
                                 Text("My App")
+                            },
+                            navigationIcon = {
+                                IconButton(onClick = { /* logic*/}) {
+                                    Icon(
+                                        imageVector = Icons.Filled.Menu,
+                                        contentDescription = "Localized description"
+                                    )
+                                }
+                            },
+                            actions = {
+                                IconButton(onClick = { /* logic*/}) {
+                                    Icon(
+                                        imageVector = Icons.Filled.MoreVert,
+                                        contentDescription = "Localized description"
+                                    )
+                                }
                             }
                         )
                     },
