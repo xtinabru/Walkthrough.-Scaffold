bf1a32b55073004c101156df73350e25d6a8267c
Cristofor4ik <xtinabruscaginxxx@gmail.com>
2025-02-17 02:53:24 +0200

    Complete 2nd part

--- app/src/main/java/com/example/walkthroughscaffold/MainActivity.kt
+++ app/src/main/java/com/example/walkthroughscaffold/MainActivity.kt
@@ -4,30 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.walkthroughscaffold.ui.theme.WalkthroughScaffoldTheme
-import kotlin.math.exp
+import com.example.walkthroughscaffold.ui.theme.composables.ScaffoldApp
+
 
 class MainActivity : ComponentActivity() {
     @OptIn (ExperimentalMaterial3Api::class)
@@ -36,55 +21,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             WalkthroughScaffoldTheme {
-                Scaffold(
-                    topBar = {
-                        var expanded by remember { mutableStateOf(false)}
-                        TopAppBar(
-                            colors = TopAppBarDefaults.topAppBarColors(
-                                containerColor = MaterialTheme.colorScheme.primaryContainer,
-                                titleContentColor = MaterialTheme.colorScheme.primary,
-                            ),
-                            title = {
-                                Text("My App")
-                            },
-                            navigationIcon = {
-                                IconButton(onClick = { /* logic*/}) {
-                                    Icon(
-                                        imageVector = Icons.Filled.Menu,
-                                        contentDescription = "Localized description"
-                                    )
-                                }
-                            },
-                            actions = {
-                                IconButton(onClick = { expanded =!expanded}) {
-                                    Icon(
-                                        imageVector = Icons.Filled.MoreVert,
-                                        contentDescription = "Localized description"
-                                    )
-                                }
-                                DropdownMenu(
-                                    expanded = expanded,
-                                    onDismissRequest = { expanded = false}
-                                ) {
-                                    DropdownMenuItem(
-                                        text = {Text("Info")},
-                                        onClick = { /* logic*/}
-                                    )
-                                    DropdownMenuItem(
-                                        text = {Text("Settings")},
-                                        onClick = { /* logic*/}
-                                    )
-                                }
-                            }
-                        )
-                    },
-                    content = { innerPadding ->
-                        Greeting(
-                            name = "Android",
-                            modifier = Modifier.padding(innerPadding)
-                        )
-                    }
-                )
+             ScaffoldApp()
             }
         }
     }
